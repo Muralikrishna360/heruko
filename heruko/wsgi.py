9a65f18fa1c8e09f52176490d792160bd013a8bc
MURALI KRISHNA <kmuralikrishna360@gmail.com>
2021-05-03 07:29:11 +0530

    changed

--- heruko/wsgi.py
+++ heruko/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heruko.settings')
 
 application = get_wsgi_application()
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
